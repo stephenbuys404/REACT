b836554f6905f2b18444cd723fc783abfb0e01a3
Stephen Buys <stephenbuys404@gmail.com>
2022-09-29 14:36:03 +0200

    PartB

---
+++ hello_hooks/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import useFetch from './useFetch';
+import Users from './Users';
+
+const App = () => {
+  const postsUrl = "https://jsonplaceholder.typicode.com/posts";
+  const todosUrl = "https://jsonplaceholder.typicode.com/todos";
+  const [requested, setRequested] = useState(postsUrl);
+  const data = useFetch(requested)
+
+  return (
+    <div>
+      <Users />
+      <Button variant="link" onClick={() => setRequested(postsUrl)}>
+        Posts
+      </Button>
+      <Button variant="link" onClick={() => setRequested(todosUrl)}>
+        Todos
+      </Button>
+      <br />
+      Requested: {requested}
+      <ul>
+        {data.map((el) => (
+          <li key={el.id}>{el.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default App;
