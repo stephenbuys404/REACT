531f1517c70b10c713243b19477482e61dcc10f0
Stephen Buys <stephenbuys404@gmail.com>
2022-09-29 14:37:45 +0200

    PartD

---
+++ reduxcart/src/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import cartReducer from "./reducer";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+var destination = document.querySelector("#container");
+var store = createStore(cartReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  destination
+);
